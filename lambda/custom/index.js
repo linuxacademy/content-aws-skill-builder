2ad5531d049665566b553108fc3430d485e40b17
Larry Fritts <lfritts@protonmail.com>
2019-11-18 10:06:56 -0800

    refactor of template

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -135,17 +135,6 @@ const ErrorHandler = {
     }
 };
 
-// This request interceptor will bind a translation function 't' to the handlerInput
-const LocalisationRequestInterceptor = {
-    process(handlerInput) {
-        i18n.init({
-            lng: Alexa.getLocale(handlerInput.requestEnvelope),
-            resources: languageStrings
-        }).then((t) => {
-            handlerInput.t = (...args) => t(...args);
-        });
-    }
-};
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've

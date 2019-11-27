73cb0c4908e6984060d0d4ed3b8bbbce6ef5fdce
Larry Fritts <lfritts@protonmail.com>
2019-11-27 09:13:10 -0800

    fix reprompt

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -73,7 +73,7 @@ const FinalCommentsIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .reprompt(speakOutput)
             .getResponse();
     }
 };
